10900c3d241201eafd947efa756893b23db632ce
Gabriele Batagiero <79870386+GabrieleBatagiero@users.noreply.github.com>
2022-02-17 09:02:52 -0300

    Add files via upload

---
+++ aula_01/index.php
@@ -0,0 +1,23 @@
+<?php
+    //comentário de uma linha
+    /*daqui pra baixo tudo é comentá<rio*/
+
+    $nome = "Gab";
+    $idade = "15";
+    $trabalhando = TRUE;
+
+    echo "Meu nome é $nome";
+    echo "<br>";
+    echo $idade;
+    echo "<br>";
+    echo $trabalhando;
+    echo "<br>";
+    $numero = "12";
+
+    echo $idade . $numero;
+    echo "<br>";
+
+    var_dump($idade);
+    echo "<br>";
+    
+?>
